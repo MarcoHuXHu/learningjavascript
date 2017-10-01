04add8bf21228751cfb6a11af7fa14e350dbd121
MarcoHuXHu <huxhu2333@gmail.com>
2017-10-02 00:34:39 +0800

    interval_task
    use interval task to update position

--- mouse spin calculate/spincontrol.js
+++ mouse spin calculate/spincontrol.js
@@ -1,5 +1,8 @@
+var curX = 0;
+var curY = 0;
 var preX = 0;
 var preY = 0;
+var curTime = undefined;
 var preTime = undefined;
 
 let pic_width  = 20;
@@ -9,6 +12,25 @@ var centerY = 300 + pic_width;
 var curRad = 0;
 var cumulate = 0;
 
+var damping_coefficient = 2;
+var mouse_end_speed = 0;
+var mouse_cur_speed = 0;
+
+var intervalID=self.setInterval("intervalTask()", 200)
+
+function intervalTask() {
+    //document.getElementById("speed").innerHTML=speed;
+    document.getElementById("x").innerHTML=preX + " : " + curX;
+    document.getElementById("y").innerHTML=preY + " : " + curY;
+    document.getElementById("rad").innerHTML=curRad;
+    if(typeof preTime != "undefined") {
+        update_speed(preX, preY, preTime, curX, curY, curTime);
+    }
+
+    preX = curX;
+    preY = curY;
+    preTime = curTime;
+}
 
 function start() {
     document.addEventListener('mousemove', showMousePosition, false);
@@ -24,58 +46,56 @@ function start() {
     
     centerX = spinX + pic_width;
     centerY = spinY + pic_width;
+
+    curX = 0;
+    curY = 0;
     preX = 0;
     preY = 0;
+    curTime = undefined;
     preTime = undefined;
-    curRad = 0;
-    cumulate = 0;
 }
 
 // using callback to get the position of mouse and update speed and cumulate
 function showMousePosition(event) {   
     // y-axle is reversed compare to normal x-y coordinate
-    var x = event.clientX - centerX;
-    var y = centerY - event.clientY;
-    var time = Date.now();
+    curX = event.clientX - centerX;
+    curY = centerY - event.clientY;
+    curTime = Date.now();
     
     // for first time move mouse
-    if(typeof preTime === "undefined"){
-        preX = x;
-        preY = y;
-        preTime = time;
-        return;
-    }
-    calc_speed(x, y, time - preTime);
-    preTime = time;
-    preX = x;
-    preY = y;
+    // if(typeof preTime === "undefined"){
+    //     preX = curX;
+    //     preY = curY;
+    //     preTime = curTime;
+    //     return;
+    // }
+    //calc_speed(curX, curY, curTime - preTime);
 }
 
 // calculate speed and update cumulate
-function calc_speed(x, y, deltaTime) {
-    var rad = - deltaRad(preX, preY, x, y);
+//function calc_speed(x, y, deltaTime) {
+// pre(x, y, t), cur(x, y, t)
+function update_speed(x1, y1, t1, x2, y2, t2) {
+    var rad = - deltaRad(x1, y1, x2, y2);
+    deltaTime = t2 - t1;
     if (Math.abs(rad) < 0.000001) return;
 
     var speed = rad / deltaTime;
     curRad = curRad + rad;
-
-    document.getElementById("x").innerHTML=x;
-    document.getElementById("y").innerHTML=y;
-    document.getElementById("rad").innerHTML=curRad;
 
     // when move counter-clockwise for a whole circle
     if (cumulate - curRad > 2 * Math.PI) curRad = curRad + 2 * Math.PI;
     // clockwise only
     if (curRad > cumulate) {
         cumulate = curRad;
+        mouse_end_speed = speed;
         document.getElementById("speed").innerHTML=speed;
         document.getElementById("cumulate").innerHTML=cumulate / 2 / Math.PI;   // Rads to Circles
     }    
 
 }
-// spin_center(x0, y0)
 // pre_position(x1, y1)
-// now_position(x2, y2)
+// cur_position(x2, y2)
 function deltaRad(x1, y1, x2, y2) {
 
     var preRad = arctan(x1, y1);
